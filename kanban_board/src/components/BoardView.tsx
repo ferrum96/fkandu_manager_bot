8d0905f925d7e304e04d1f2d8c185287df6d2653
Konstantin <ferrumsk96@gmail.com>
2026-06-30 20:29:30 +0300

    fix: change button label from '+ Задача' to 'Новая задача'

--- kanban_board/src/components/BoardView.tsx
+++ kanban_board/src/components/BoardView.tsx
@@ -122,7 +122,7 @@ export function BoardView({
               onClick={() => handleAddTask(board.columns[0]?.id)}
               className="px-4 py-2 text-sm bg-blue-600 text-white rounded-lg hover:bg-blue-700"
             >
-              + Задача
+              Новая задача
             </button>
           </div>
         </div>
